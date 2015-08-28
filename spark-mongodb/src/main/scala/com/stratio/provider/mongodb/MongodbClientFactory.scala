9a86ae7f8d10bbe1613d15a917094e334bf0086d
Steven Fines <sfines@objectdriven.com>
2015-08-27 22:45:06 -0700

    Updated pom.xml for scala 2.11
    Added additional connection parameters to the MongodbConfig and then enabled the reader and client factory for those configuration options

--- spark-mongodb/src/main/scala/com/stratio/provider/mongodb/MongodbClientFactory.scala
+++ spark-mongodb/src/main/scala/com/stratio/provider/mongodb/MongodbClientFactory.scala
@@ -5,6 +5,7 @@ import javax.net.ssl.SSLSocketFactory
 import com.mongodb.ServerAddress
 import com.mongodb.casbah.Imports._
 import com.mongodb.casbah.{MongoClient, MongoClientOptions}
+import org.joda.time.field.MillisDurationField
 
 /**
  * Different client configurations to Mongodb database
@@ -15,54 +16,60 @@ object MongodbClientFactory {
 
   def createClient(host : String) : Client = MongoClient(host)
 
-  def createClient(host : String, port : Int, user: String, database: String, password :String) : Client = {
-
+  def createClient(host:String, port:Int, user:String,database:String, password:String ):Client = {
     val credentials : List[MongoCredential]= List(MongoCredential.createCredential(user, database, password.toCharArray))
     val hostPort = new ServerAddress(host, port)
-    MongoClient(hostPort, credentials)
+
+    createClient(List(hostPort), credentials)
   }
 
-  def createClient(hostPort : List[ServerAddress], credentials : List[MongoCredential]) : Client = MongoClient(hostPort, credentials)
+  def createClient( hostPort : List[ServerAddress],
+                    credentials : List[MongoCredential] = List.empty,
+                    optionSSLOptions: Option[MongodbSSLOptions] = None,
+                    readPreference: String = "nearest",
+                    options:Map[String, String] = Map.empty) : Client = {
 
-  def createClient(hostPort : List[ServerAddress], credentials : List[MongoCredential], readPreference: String) : Client = {
-    val options = new MongoClientOptions.Builder().readPreference(parseReadPreference(readPreference)).build()
-    MongoClient(hostPort, credentials, options)
-  }
-
-  def createClient(
-                    hostPort : List[ServerAddress],
-                    credentials : List[MongoCredential],
-                    optionSSLOptions: Option[MongodbSSLOptions]) : Client = {
+    val optionsBuilder = new MongoClientOptions.Builder()
 
     if (sslBuilder(optionSSLOptions)) {
-
-      val options = new MongoClientOptions.Builder().socketFactory(SSLSocketFactory.getDefault()).build()
-      MongoClient(hostPort, credentials, options)
-    }
-    else
-      MongoClient(hostPort, credentials)
-
-  }
-
-  def createClient(
-    hostPort : List[ServerAddress],
-    credentials : List[MongoCredential],
-    optionSSLOptions: Option[MongodbSSLOptions],
-    readPreference: String) : Client = {
-
-    if (sslBuilder(optionSSLOptions)) {
-      val options = new MongoClientOptions.Builder()
-        .readPreference(parseReadPreference(readPreference))
-        .socketFactory(SSLSocketFactory.getDefault()).build()
-
-      MongoClient(hostPort, credentials, options)
-    }
-    else {
-      val options = new MongoClientOptions.Builder().readPreference(parseReadPreference(readPreference)).build()
-
-      MongoClient(hostPort, credentials, options)
+      optionsBuilder.socketFactory(SSLSocketFactory.getDefault())
     }
 
+    optionsBuilder.readPreference(parseReadPreference(readPreference))
+
+    if( options.get(MongodbConfig.ConnectTimeoutMillis).isDefined) {
+      optionsBuilder.connectTimeout(options.get(MongodbConfig.ConnectTimeoutMillis).get.toInt)
+    }
+
+    if( options.get(MongodbConfig.SocketTimeoutMillis).isDefined) {
+      optionsBuilder.socketTimeout(options.get(MongodbConfig.SocketTimeoutMillis).get.toInt)
+    }
+
+    if( options.get(MongodbConfig.ReplicaSet).isDefined){
+      optionsBuilder.requiredReplicaSetName(options.get(MongodbConfig.ReplicaSet).get)
+    }
+
+    if( options.get(MongodbConfig.MinPoolSize).isDefined){
+      optionsBuilder.minConnectionsPerHost(options.get(MongodbConfig.MinPoolSize).get.toInt)
+    }
+
+    if( options.get(MongodbConfig.MaxPoolSize).isDefined){
+      optionsBuilder.connectionsPerHost(options.get(MongodbConfig.MaxPoolSize).get.toInt)
+    }
+
+    if( options.get(MongodbConfig.MaxIdleTimeMillis).isDefined){
+      optionsBuilder.maxConnectionIdleTime(options.get(MongodbConfig.MaxIdleTimeMillis).get.toInt)
+    }
+
+    if( options.get(MongodbConfig.WaitQueueMultiple).isDefined){
+      optionsBuilder.threadsAllowedToBlockForConnectionMultiplier(options.get(MongodbConfig.WaitQueueMultiple).get.toInt)
+    }
+
+    if( options.get(MongodbConfig.AutoConnectRetry).isDefined){
+      optionsBuilder.autoConnectRetry(options.get(MongodbConfig.AutoConnectRetry).get.toBoolean)
+    }
+
+    MongoClient(hostPort, credentials, optionsBuilder.build())
   }
 
   private def sslBuilder(optionSSLOptions: Option[MongodbSSLOptions]): Boolean = {
