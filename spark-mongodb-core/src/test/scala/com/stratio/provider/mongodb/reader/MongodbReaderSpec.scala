49cabbecea9299c5c5324d2000638499ec5f9c1a
pmadrigal <pedromadri89@hotmail.com>
2015-10-06 16:48:25 +0200

    license and anotation changed in test

--- spark-mongodb-core/src/test/scala/com/stratio/provider/mongodb/reader/MongodbReaderSpec.scala
+++ spark-mongodb-core/src/test/scala/com/stratio/provider/mongodb/reader/MongodbReaderSpec.scala
@@ -1,23 +1,18 @@
-/*
+/**
+ * Copyright (C) 2015 Stratio (http://stratio.com)
  *
- *  Licensed to STRATIO (C) under one or more contributor license agreements.
- *  See the NOTICE file distributed with this work for additional information
- *  regarding copyright ownership. The STRATIO (C) licenses this file
- *  to you under the Apache License, Version 2.0 (the
- *  "License"); you may not use this file except in compliance
- *  with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied. See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
- * /
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package com.stratio.provider.mongodb.reader
 
 import java.sql.Timestamp
@@ -32,8 +27,11 @@ import com.stratio.provider.partitioner.PartitionRange
 import org.apache.spark.sql.sources.{EqualTo, Filter}
 import org.apache.spark.sql.test.TestSQLContext
 import org.apache.spark.sql.types._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FlatSpec, Matchers}
 
+@RunWith(classOf[JUnitRunner])
 class MongodbReaderSpec extends FlatSpec
 with Matchers
 with MongoEmbedDatabase
